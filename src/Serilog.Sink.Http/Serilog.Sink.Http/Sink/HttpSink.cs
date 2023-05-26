633354450d3ead6170f5cbb82618cfe2760d4c23
lc <y-y-liu@hotmail.com>
2023-05-26 20:14:13 +0800

    修复创建HttpClient不释放问题

--- src/Serilog.Sink.Http/Serilog.Sink.Http/Sink/HttpSink.cs
+++ src/Serilog.Sink.Http/Serilog.Sink.Http/Sink/HttpSink.cs
@@ -1,24 +1,30 @@
-﻿using Newtonsoft.Json;
-using Serilog.Core;
+﻿using Serilog.Core;
 using Serilog.Events;
 using Serilog.Sink.Http.Models;
 using System;
 using System.Net.Http;
 using System.Text;
+using System.Text.Json;
 
 namespace Serilog.Sink.Http.Sink
 {
     public class HttpSink : ILogEventSink
     {
+        private readonly HttpClient _httpClient;
         private readonly Uri _uri;
         private readonly IFormatProvider _formatProvider;
 
-        public HttpSink(Uri uri, IFormatProvider formatProvider)
+        public HttpSink()
+        {
+            _httpClient = new HttpClient();
+        }
+
+        public HttpSink(Uri uri, IFormatProvider formatProvider) : this()
         {
             _uri = uri;
             _formatProvider = formatProvider;
         }
-        public HttpSink(string url, IFormatProvider formatProvider)
+        public HttpSink(string url, IFormatProvider formatProvider) : this()
         {
             _uri = new Uri(url);
             _formatProvider = formatProvider;
@@ -29,9 +35,13 @@ namespace Serilog.Sink.Http.Sink
         {
             var message = logEvent.RenderMessage(_formatProvider);
             //Console.WriteLine(DateTimeOffset.Now.ToString() + " " + message);
-            var req = new HttpLogEvent(logEvent.Level, message);
-            var s = JsonConvert.SerializeObject(req);
-            await new HttpClient().PostAsync(_uri, new StringContent(s, Encoding.UTF8, "application/json"));
+            var model = new HttpLogEvent(logEvent.Level, message);
+            var buffer = JsonSerializer.SerializeToUtf8Bytes(model);
+            var content = new ByteArrayContent(buffer);
+            var request = new HttpRequestMessage(HttpMethod.Post, _uri);
+            request.Content = content;
+            request.Headers.Add("Content-Type", "application/json");
+            await _httpClient.SendAsync(request);
         }
     }
 }
